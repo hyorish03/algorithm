2ac62ce0d5f5b33184addf8c6898dc86590e2b6c
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2023-04-26 14:05:22 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+
 int main() {
 	int a, b, c, temp;
 
@@ -9,21 +10,14 @@ int main() {
 
 	if (a == b && b == c)
 		cout << 10000 + a * 1000;
-	else if ((a == b || a == c) && b != c)
+	else if (a == b || a == c)
 		cout << 1000 + a * 100;
 
-	else if (b == c && a != b)
+	else if (b == c)
 		cout << 1000 + b * 100;
 	else {
-		if (a > b)
-			temp = a;
-		else
-			temp = b;
-
-		if (temp < c)
-			temp = c;
-
-		cout << temp * 100;
+		
+		cout << max(max(a, b), c) * 100;
 	}
 	
 }

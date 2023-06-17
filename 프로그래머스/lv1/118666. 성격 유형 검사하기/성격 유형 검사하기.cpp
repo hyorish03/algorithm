b234a366f2c92300c079aeaeb119106419d81f44
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2023-06-17 18:54:59 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.05 ms, Memory: 3.79 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+int score[8] = {0, 3, 2, 1, 0, 1,2, 3};
+string solution(vector<string> survey, vector<int> choices) {
+    map<char, int> m;
+        string answer = "";
+
+    for(int i = 0; i < survey.size(); i++){
+        string sur = survey[i];
+        int ans = choices[i];
+        
+        m[sur[ans/4]] += score[ans];
+    }
+    
+    answer += m['R'] >= m['T'] ? "R"  :"T";
+    answer += m['C'] >= m['F'] ? "C"  :"F";
+    answer += m['J'] >= m['M'] ? "J"  :"M";
+    answer += m['A'] >= m['N'] ? "A"  :"N";
+    
+    
+    return answer;
+}

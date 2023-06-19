57358b637e525100981c1ec24296690e4eda8f3e
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2023-06-19 14:52:33 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.07 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -1,25 +1,31 @@
 #include <string>
 #include <vector>
 #include <map>
-
 using namespace std;
-int score[8] = {0, 3, 2, 1, 0, 1,2, 3};
-string solution(vector<string> survey, vector<int> choices) {
-    map<char, int> m;
-        string answer = "";
 
+string solution(vector<string> survey, vector<int> choices) {
+    int score[8] = {0, 3, 2, 1, 0, 1, 2, 3};
+    map<char, int> m;
+    string sur;
+    string answer = "";
+    int c;
+    
     for(int i = 0; i < survey.size(); i++){
-        string sur = survey[i];
-        int ans = choices[i];
+        sur = survey[i];
+        c = choices[i];
         
-        m[sur[ans/4]] += score[ans];
+         if(c < 4){
+            m[sur[0]] += score[c];
+         } else if(c >= 4){
+            m[sur[1]] += score[c];
+         }   
     }
     
-    answer += m['R'] >= m['T'] ? "R"  :"T";
-    answer += m['C'] >= m['F'] ? "C"  :"F";
-    answer += m['J'] >= m['M'] ? "J"  :"M";
-    answer += m['A'] >= m['N'] ? "A"  :"N";
-    
+   answer += m['R'] >= m['T'] ? 'R' : 'T'; 
+   answer += m['C'] >= m['F'] ? 'C' : 'F'; 
+   answer += m['J'] >= m['M'] ? 'J' : 'M'; 
+   answer += m['A'] >= m['N'] ? 'A' : 'N'; 
+        
     
     return answer;
 }

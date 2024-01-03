8942edcea494fdd15d05d7f5909338d7cc23dba9
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-01-04 00:23:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,49 +1,49 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 #include <queue>
-#include <string.h>
+#define MAX 100001
+
 using namespace std;
+int st, en;
+int visited[MAX];
+vector<int> ans;
+queue<int> q;
 
-const int MAX = 100001;
-int visited[MAX] = { 0, };
-int map[MAX] = { 0, };
-
-int bfs(int start, int end) {
-	queue <int> q;
-	q.push(start);
-	visited[start] = 1;
-
-	while (!q.empty()) {
-		int here = q.front();
-		int cnt = visited[here];
-		q.pop();
-
-		if (here == end) {
-			return cnt;
-		}
-
-		if (  here + 1 < MAX && visited[here + 1] == 0) {
-			visited[here + 1] = cnt + 1;
-			q.push(here + 1);
-		}
-
-		if ( here - 1 > 0 && visited[here - 1] == 0) {
-			visited[here - 1] = cnt + 1;
-			q.push(here - 1);
-		}
-
-		if ( here * 2 < MAX && visited[here * 2] == 0 ) {
-			visited[here * 2] = cnt + 1;
-			q.push(here * 2);
-		}
-	}
+void bfs(int st){
+    visited[st] = 0;
+    q.push(st);
+  
+    
+    while(!q.empty()){
+        st = q.front();
+        q.pop();
+        
+        if(st == en){
+            return;
+        }
+        
+        if(st + 1 < MAX && visited[st+1] == false){
+            visited[st+1] = visited[st] + 1;
+            q.push(st+1);
+        }
+        
+        if(st - 1 >= 0 && visited[st-1] == false){
+            visited[st-1] = visited[st] + 1;
+            q.push(st-1);
+        }
+        
+        if(st * 2 < MAX && visited[st * 2] == false){
+            visited[st * 2] = visited[st] + 1;
+            q.push(st * 2);
+        }
+    }
 }
 
 int main() {
-	int N, K; // N: 수빈이가 있는 위치, K ; 동생이 있는 위치
-	cin >> N >> K;
-
-	if (N > K)
-		cout << N - K << "\n";
-	else
-		cout << bfs(N, K) - 1 << "\n";
+    cin >> st >> en;
+    
+    bfs(st);
+    
+    cout << visited[en];
 }

0be0d264ad4397241d6c743d58dcef1662c702d5
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-01-29 22:47:16 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 4 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
+#define MAX 1000001
+#define INF 9999999
+using namespace std;
+
+int n;
+long long dp[MAX];
+
+vector<int> ans;
+vector<int> result;
+
+long long dfs(int n){
+    long long &ret = dp[n];
+    
+    if(ret != -1){
+        return ret;
+    }
+    
+    
+    ret = dfs(n-1) + dfs(n-2);
+    return ret;
+}
+
+int main()
+{
+    FASTIO;
+    cin >> n;
+    
+    memset(dp, -1, sizeof dp);
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    
+    if(n < 3){
+        cout << dp[n];
+    } else {
+        long long ret = dfs(n);
+        cout << ret;
+    }
+}

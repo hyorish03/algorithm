1ac2ddcdbaa1fabf3eb6e643cb7b22646673d8ea
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-01-31 16:47:42 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -5,5 +5,18 @@
 
 SELECT I.Animal_id, i.animal_type, i.name from animal_ins as i
 inner join animal_outs as o on o.animal_id = i.animal_id
-where i.sex_upon_intake != o.sex_upon_outcome 
+where i.sex_upon_intake like "Intact%" and SEX_UPON_OUTCOME IN('Neutered Male','Spayed Female')
 order by i.animal_id
+
+# -- 코드를 입력하세요 ANIMAL_ID, ANIMAL_TYPE,NAME 
+# SELECT A.ANIMAL_ID 
+#       ,A.ANIMAL_TYPE
+#       ,A.NAME
+# FROM ANIMAL_INS AS A
+# INNER JOIN (
+#            SELECT ANIMAL_ID, SEX_UPON_OUTCOME
+#            FROM ANIMAL_OUTS 
+#            WHERE SEX_UPON_OUTCOME IN('Neutered Male','Spayed Female')
+#             ) AS B ON A.ANIMAL_ID = B.ANIMAL_ID
+# WHERE A.SEX_UPON_INTAKE NOT IN ('Neutered Male','Spayed Female')
+# ORDER BY 1

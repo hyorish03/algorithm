3eb173ab497cc3980a6825a3b81d2399e3f84a59
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-02-01 14:37:10 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -2,22 +2,24 @@
 #include <vector>
 #include <cstring>
 #define MAX 1001
+#define FASTIO ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 
-int dp[MAX];
-int dfs(int n){
-    int &ret = dp[n];
-
-    if(ret != -1){
-        return ret;
-    }
-
-    ret = 1+min(dfs(n-1), dfs(n-3));
-    return ret;
-}
-
+//int dp[MAX];
+//int dfs(int n){
+//    int &ret = dp[n];
+//
+//    if(ret != -1){
+//        return ret;
+//    }
+//
+//    ret = 1+min(dfs(n-1), dfs(n-3));
+//    return ret;
+//}
+//
 
 int main(){
+    FASTIO;
     int n;
     cin >> n;
    

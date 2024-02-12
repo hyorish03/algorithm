4747c9a1472700c151846c16f81242b4f11e7e8d
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-02-12 22:15:33 +0900

    [Gold V] Title: AC, Time: 36 ms, Memory: 3112 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 #define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 #define MAX 10000001
 using namespace std;
@@ -15,11 +16,10 @@ int main() {
         cin >> n;
         cin >> nums;
         
-        vector<int> arr;
-        int num;
+        deque<int> arr;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] >= '0' && nums[i] <= '9'){
-                num = 0;
+                int num = 0;
                 while(i < nums.size() && nums[i] >= '0' && nums[i] <= '9'){
                     num = num * 10 + (nums[i] - '0');
                     i++;
@@ -36,7 +36,6 @@ int main() {
                 reverse_flag = !reverse_flag;
             } else if(op == 'D'){
                 if(start == end){
-                    cout << "error" << '\n';
                     error = true;
                     break;
                 }
@@ -48,22 +47,26 @@ int main() {
             }
         }
         
-        if(!error){
+        if(error || start > end){
+            cout << "error\n";
+        } else {
             cout << '[';
-            if(start <= end){
-                if(reverse_flag){
-                    for(int i = end - 1; i >= start; i--){
-                        cout << arr[i];
-                        if(i != start) cout << ',';
+            if(reverse_flag){
+                for(int i = end - 1; i >= start; i--){
+                    cout << arr[i];
+                    if(i != start){
+                        cout << ',';
                     }
-                } else {
-                    for(int i = start; i < end; i++){
-                        cout << arr[i];
-                        if(i != end - 1) cout << ',';
+                }
+            } else {
+                for(int i = start; i < end; i++){
+                    cout << arr[i];
+                    if(i != end - 1){
+                        cout << ',';
                     }
                 }
             }
-            cout << ']' << '\n';
+            cout << "]\n";
         }
     }
     

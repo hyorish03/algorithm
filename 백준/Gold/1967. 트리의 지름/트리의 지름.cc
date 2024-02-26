eed3578344e54712fdfd4b16ddd63c7ee806e450
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-02-26 19:39:15 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 3036 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(nullptr);cout.tie(nullptr);
+#define MAX 10001
+using namespace std;
+vector<int> ans;
+int vis[MAX] = {0,};
+int maxDist = -1, maxNode = 1;
+
+struct Node {
+    int node;
+    int dist;
+};
+
+vector<vector<Node>> v(MAX);
+
+void dfs(int start, int dist){
+    if(vis[start]){
+        return;
+    }
+    
+    if(maxDist <= dist){
+        maxDist = dist;
+        maxNode = start;
+    }
+    
+    vis[start] = true;
+    
+    for(int i = 0; i < v[start].size(); i++){
+        int nextNode = v[start][i].node;
+        int nextDist = v[start][i].dist;
+        
+        dfs(nextNode, nextDist + dist);
+    }
+    
+}
+
+int main() {
+    FASTIO;
+    
+    int n; // 노드의 개수
+    cin >> n;
+    
+    for(int i = 0; i < n-1; i++){
+        int from, to, dist;
+        cin >> from >> to >> dist;
+        v[from].push_back({to, dist});
+        v[to].push_back({from, dist});
+
+    }
+    
+//    for(int i = 1; i <= n; i++){
+        dfs(1,0);
+        memset(vis, 0, sizeof(vis));
+        dfs(maxNode, 0);
+    cout << maxDist;
+//    }
+    return 0;
+}

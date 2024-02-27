a9274b82db3b2de6f6787cb181d1e1155f28a977
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-02-27 10:29:45 +0900

    [Gold V] Title: 노드사이의 거리, Time: 8 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,50 +1,81 @@
 #include <iostream>
-#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
 #include <cstring>
-#define MAX 10001
+#include <vector>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(nullptr);cout.tie(nullptr);
 #define INF 987654321
-#define FASTIO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define MAX 1001
 using namespace std;
+vector<int> ans;
+int vis[MAX] = {0,};
+struct Node {
+    int node;
+    int dist;
+};
 
-vector<vector<pair<int, int>>> v(MAX);
-int vis[MAX];
-
-int dfs(int node, int dest, int dist) {
+int start, dest, result = MAX, minDist = 0;
+int n, m;
+vector<vector<Node>> v(MAX);
+int dfs(int node, int dest, int dist){
+//    cout << "node: " << node << "dist: " << dist << endl;
     vis[node] = true;
     
-    if (node == dest)
+    if(node == dest){
         return dist;
-    
+    }
     int minDist = INF;
-    for (auto next : v[node]) {
-        int nextNode = next.first;
-        int nextDist = next.second;
-        if (!vis[nextNode]) {
-            int res = dfs(nextNode, dest, dist + nextDist);
-            minDist = min(minDist, res);
+    for(int i = 0; i < v[node].size(); i++){
+        int nextNode = v[node][i].node;
+        int nextDist = v[node][i].dist;
+        if(!vis[nextNode]){
+            int res = dfs(nextNode, dest, nextDist+dist);
+            result = min(minDist, res);
         }
     }
-    return minDist;
+    return result;
 }
 
 int main() {
     FASTIO;
-    int n, m;
+    int from, to, dist;
+
     cin >> n >> m;
-    
-    for (int i = 0; i < n - 1; i++) {
-        int from, to, dist;
+    for(int i = 0; i < n-1; i++){
         cin >> from >> to >> dist;
         v[from].push_back({to, dist});
         v[to].push_back({from, dist});
     }
     
-    for (int i = 0; i < m; i++) {
-        memset(vis, 0, sizeof(vis));
-        int start, dest;
+    for(int i = 0; i < m; i++){
         cin >> start >> dest;
+        memset(vis, 0, sizeof(vis));
         cout << dfs(start, dest, 0) << '\n';
     }
     
     return 0;
 }
+//4 2
+//2 1 2
+//4 3 2
+//1 4 3
+//3 1
+//4 2
+
+/**
+ 12 2
+ 1 2 3
+ 1 3 2
+ 2 4 5
+ 3 5 11
+ 3 6 9
+ 4 7 1
+ 4 8 7
+ 5 9 15
+ 5 10 4
+ 6 11 6
+ 6 12 10
+ 1 4
+ 3 6
+ */

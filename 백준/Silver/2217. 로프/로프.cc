b661e7e75252ce43be1d90001458076678f13f20
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-03-05 14:46:01 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define INF 987654321
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end(), greater<int>());
+    
+    int ans = 0;
+    int sum = INF;
+    for(int i = 0; i < n; i++){
+        sum = min(sum, v[i]);
+        int temp = sum * (i+1);
+        ans = max(ans, temp);
+    }
+    cout << ans;
+    
+    return 0;
+}

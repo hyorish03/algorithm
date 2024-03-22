0c6525f6a12f8ca19d27c46aa4a909114a462128
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-03-22 15:53:22 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 8 ms, Memory: 2148 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -11,31 +11,37 @@ int vis[MAX][MAX];
 int dx[] ={0, 0, 1, -1};
 int dy[] ={1, -1, 0, 0};
 int n;
-
-bool valid(int ny, int nx){
-    if(ny < n && nx < n && ny >= 0 && nx >= 0) return true;
-    else return false;
-    
-}
 int idx = 1;
-void bfs(int y, int x){
-    queue<pair<int,int>> q;
-    q.push({y, x});
-    vis[y][x] = arr[y][x];
+
+struct Q {
+    int cost;
+    int y;
+    int x;
     
-    while(!q.empty()){
-        int y = q.front().first;
-        int x = q.front().second;
-        q.pop();
+    bool operator<(const Q& other) const {
+        return cost > other.cost; // 비용이 작은 것이 먼저 오도록
+    }
+};
+void bfs(int y, int x){
+    
+    priority_queue<Q, vector<Q>> pq;
+    vis[y][x] = arr[y][x];
+    pq.push({arr[y][x], y, x});
+    
+    while(!pq.empty()){
+        int y = pq.top().y;
+        int x = pq.top().x;
+        int cost = pq.top().cost;
+        pq.pop();
         
         for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
             if(ny < n && nx < n && ny >= 0 && nx >= 0){
-                int temp = vis[y][x] + arr[ny][nx];
+                int temp = cost + arr[ny][nx];
                 if(vis[ny][nx] > temp){
                     vis[ny][nx] = temp;
-                    q.push({ny, nx});
+                    pq.push({temp,ny, nx});
                 }
             }
         }
@@ -60,7 +66,6 @@ int main() {
         }
         
         bfs(0,0);
-        
         cout << "Problem " << idx << ": " << vis[n-1][n-1] << '\n';
         idx++;
     }

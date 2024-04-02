57b062c571f0609d86914b4cfbc8aace78af89f3
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-04-02 17:10:50 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,32 +1,24 @@
-#include<iostream>
-#include<algorithm>
-#include <ctime>
-#include <cstring>
-#define MAX 1000001
-#define INF 9999999
+#include <iostream>
 using namespace std;
-int arr[MAX];
-int psm[MAX];
-int sum = 0;
 
-int main()
-{
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(false);
-    int m;
-    int n, st, en;
-    cin >> n;
-    cin >> m;
+int s[100001];
+int n, m, a;
+int main() {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n >> m;
     for(int i = 1; i <= n; i++){
-        cin >> arr[i];
-        psm[i] = psm[i-1] + arr[i];
+        cin >> a;
+        s[i] = s[i-1] + a;
     }
-        
-    for(int i = 0; i < m; i++ ){
+    
+    for(int i = 1; i <= m; i++){
+        int st, en;
         cin >> st >> en;
-        cout << psm[en] - psm[st-1] << '\n';
+        cout << s[en] - s[st-1] << '\n';
     }
     return 0;
 }
-

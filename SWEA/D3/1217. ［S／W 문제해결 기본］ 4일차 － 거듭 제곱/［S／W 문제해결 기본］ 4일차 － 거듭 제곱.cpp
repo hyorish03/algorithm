395f10dd3f3f1704b70fb9ee8849564f1f4ffabd
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-04-04 17:58:25 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 8 ms, Memory: 13,372 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.cpp
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 
 using namespace std;
-//
-//
-//int answer(int low, int up){
-//    int ans = low;
-//    while(up > 1){
-//        if(up % 2 == 0) {
-//            ans *= ans;
-//            up /= 2;
-//        } else {
-//            ans *= low;
-//            up -= 1;
-//        }
-//        
-//    }
-//    return ans;
-//}
 
-int recur(int low, int up, int ans) {
-//    cout << up << ' ' << ans << endl;
-    if (up == 1) {
-        return ans;
+
+int answer(int low, int up){
+    int ans = 1;
+    while(up > 0){
+        if(up % 2 == 1) {
+            ans *= low;
+        }
+        low *= low;
+        up /= 2;
+//        cout << ans << ' ' << up << endl;
     }
-    return recur(low, up - 1, ans * low);
-    
+    return ans;
 }
+//
+//int recur(int low, int up, int ans) {
+////    cout << up << ' ' << ans << endl;
+//    if (up == 1) {
+//        return ans;
+//    }
+//    return recur(low, up - 1, ans * low);
+//
+//}
 
 int main() {
     
@@ -34,14 +32,14 @@ int main() {
     cout.tie(NULL);
     int tc;
 //    cin >> tc;
-    tc = 10;
-    for(int idx = 1; idx <= tc; idx++){
+    tc = 1;
+    for(int idx = 1; idx <= 10; idx++){
         int a, b;
         int t;
         cin >> t;
         cin >> a >> b;
         
-        cout << "#" << idx << ' '<< recur(a, b, a) << endl;
+        cout << "#" << idx << ' '<< answer(a, b) << endl;
     }
         
 

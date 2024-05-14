53d38d56cc177036e08accc301195047f3fcf5ec
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-05-14 13:09:23 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<string>> t;
+vector<string> answer, path;
+bool vis[10001] = {0,};
+
+void dfs(string st){
+    if(path.size() == t.size()+1){
+        if(answer.empty() || path < answer) {
+            answer = path;
+        }
+        path.pop_back();
+        return;
+    }
+    
+    for(int i = 0; i < t.size(); i++){
+        if(!vis[i] && t[i][0] == st){
+            vis[i] = true;
+            path.push_back(t[i][1]);
+            dfs(t[i][1]);
+            vis[i] = false;
+        }
+    }
+    path.pop_back();
+}
+
+
+vector<string> solution(vector<vector<string>> tickets) {
+    t = tickets;
+    sort(t.begin(), t.end()); 
+    path.push_back("ICN");
+    dfs("ICN");
+    return answer;
+}

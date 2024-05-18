1450f9dc2c156f1505a26e640f6724622bd5f201
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2024-05-19 00:57:05 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.07 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,58 +1,50 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <iostream>
 #include <cstring>
+#include <queue>
 #include <cmath>
-using namespace std;
-#define MAX 201
-vector<vector<int>> v(MAX);
-bool vis[101] = {false,};
-int s = 1;
-int len = 0;
-void bfs(int a, int b){
-    queue<int> q;
-    vis[a] = true;
-    vis[b] = true;
-    q.push(a);
 
+using namespace std;
+vector<vector<int>> v(202);
+int s = 1;
+int vis[202];
+void bfs(int x, int y){
+    queue<int> q;
+    q.push(x);
+    vis[x] = true;
+    vis[y] = true;
+    
     while(!q.empty()){
-        int x = q.front();
+        int f = q.front();
         q.pop();
-        
-        for(int i = 0; i < v[x].size(); i++){
-            if(vis[v[x][i]]) continue;
-            s++;
-            q.push(v[x][i]);
-            vis[v[x][i]] = true;
+        for(int i = 0; i < v[f].size(); i++){
+            int next = v[f][i];
+            if(!vis[next]){
+                q.push(next);
+                vis[next] = true;
+                s++;
+            }
         }
-        
     }
 }
-
 int solution(int n, vector<vector<int>> wires) {
+    
+    for(int i = 0; i < wires.size(); i++){
+        int a = wires[i][0];
+        int b = wires[i][1];
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    
     int answer = 100;
-    int point = 1;
-    len = n-1;
     for(int i = 0; i < wires.size(); i++){
         int x = wires[i][0];
         int y = wires[i][1];
-        v[x].push_back(y);
-        v[y].push_back(x);
-    }
-    
-    for(int i = 0; i < n-1; i++){
-        memset(vis, false, sizeof vis);
-
-        int a = wires[i][0];
-        int b = wires[i][1];
         s = 1;
-        cout << "A: " << a << " B: " << b ;
-        bfs(a, b);
-        answer = min(answer, abs(n-s-s));
-        cout << "ANS: " << answer << " S: " << abs(n-s-s) << '\n';
+        memset(vis, false, sizeof vis);
+        bfs(x, y);
+        answer = min(answer, abs(n-s*2));
     }
-    
- 
     return answer;
 }

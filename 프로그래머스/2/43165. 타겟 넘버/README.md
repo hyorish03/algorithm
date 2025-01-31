97a102892f10476557cf9846a6283763bfb0be0d
Hyorin_Lee <108210492+hyorish03@users.noreply.github.com>
2025-01-31 21:33:55 +0900

    [level 2] Title: 타겟 넘버, Time: 60.99 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 84.66 ms
+메모리: 37.8 MB, 시간: 60.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 10:33:09
+2025년 01월 31일 21:33:53
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,28 +1,29 @@
-var ans = 0;
-var nums;
-var tg;
+let tg = 0;
+let nums = [];
+let answer = 0;
 
-function dfs(cur, idx, status){
-
-    if(idx === nums.length -1){
-        if(cur === tg){
-            ans++;
-            
-        }
-        
+function dfs(cur, index, debug) {
+    if(index == nums.length - 1){
+        // console.log("ㅠㅠ")
+        if(cur == tg){
+        // console.log("야호 타겟이다")
+        answer++;
         return;
     }
-    
-    dfs(cur+nums[idx+1], idx+1, cur + "+" + nums[idx+1]);
-    dfs(cur-nums[idx+1], idx+1, cur + "-" + nums[idx+1]);
+        return;
+    }
+
+    dfs(cur+nums[index+1], index+1, `${debug} + "+" + ${nums[index+1]}`)
+    dfs(cur-nums[index+1], index+1, `${debug} + "-" + ${nums[index+1]}`)
     
 }
+
 function solution(numbers, target) {
-    nums = numbers;
     tg = target;
-        
-    dfs(0, -1);
+    nums = numbers;
     
-    return ans;
+    dfs(numbers[0], 0, numbers[0]);
+    dfs(-numbers[0], 0, -numbers[0]);
     
+    return answer;
 }
